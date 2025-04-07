bb1af9edbde69d1dc7c7e00c8305939b596a1709
monte <alejandromonterodeltoro@gmail.com>
2025-04-07 03:24:15 +0200

    Update

--- src/pages/ServerConfig.tsx
+++ src/pages/ServerConfig.tsx
@@ -893,7 +893,7 @@ useEffect(() => {
                               {/* Barra de progreso grande y métricas */}
                               <div style={{ marginBottom: '1rem' }}>
                                 <div style={{ 
-                                  height: '20px',
+                                  height: '21px',
                                   borderRadius: '7px',
                                   background: 'rgba(255, 255, 255, 0.1)',
                                   overflow: 'hidden',
